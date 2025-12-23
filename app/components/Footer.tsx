930e14be654401f7086604db0f75f4b8d2c9afef
ahmedaryf <ahmed.areef@hotmail.com>
2025-12-23 05:54:45 +0500

    footer updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,42 +1,37 @@
-import { client } from "@/sanity/lib/client";
+"use client";
+
 import { MailIcon } from "lucide-react";
 import Link from "next/link";
 import { AiFillFacebook, AiFillInstagram } from "react-icons/ai";
 
 import { FaPhone } from "react-icons/fa6";
 import ThemeToggler from "./ThemeToggler";
+import { usePathname } from "next/navigation";
 // import { IoIosMail } from "react-icons/io";
 
-async function getFormLinks() {
-  const query = `*[_type == "quickLinks"] | order(linkName asc){
-      link,
-      linkName
-  }`;
-  const data = await client.fetch(query, {}, { next: { revalidate: 60 } });
-  return data;
-}
-
-export default async function Footer() {
+export default function Footer() {
   const year = new Date().getFullYear();
-  const links = await getFormLinks();
+  const path = usePathname();
 
   return (
-    <footer className='min-h-[60vh] w-screen bg-primary dark:bg-black/70 text-white dark:text-secondary-foreground duration-500 dark:border-t border-white  flex flex-col justify-between py-6 '>
-      <div className=' md:flex justify-between items-start px-10 w-full xl:w-[80vw] mx-auto mt-12'>
-        <div className='flex md:hidden gap-6 justify-center items-center'>
-          <Link href={"https://www.facebook.com/vaavukeyodhoo"}>
-            <AiFillFacebook size={32} />
-          </Link>
-          <Link href={"https://www.instagram.com/visitkeyodhoo/"}>
-            <AiFillInstagram size={32} />
-          </Link>
-        </div>
-        <div className='mt-12 md:mt-0'>
-          {/* <h1 className='text-xl font-bold mb-2 md:mb-4 tracking-wider'>
+    <>
+      {!path.startsWith("/admin") && !path.startsWith("/rocknreel") ? (
+        <footer className='min-h-[60vh] w-screen bg-primary dark:bg-black/70 text-white dark:text-secondary-foreground duration-500 dark:border-t border-white  flex flex-col justify-between py-6 '>
+          <div className=' md:flex justify-between items-start px-10 w-full xl:w-[80vw] mx-auto mt-12'>
+            <div className='flex md:hidden gap-6 justify-center items-center'>
+              <Link href={"https://www.facebook.com/vaavukeyodhoo"}>
+                <AiFillFacebook size={32} />
+              </Link>
+              <Link href={"https://www.instagram.com/visitkeyodhoo/"}>
+                <AiFillInstagram size={32} />
+              </Link>
+            </div>
+            <div className='mt-12 md:mt-0'>
+              {/* <h1 className='text-xl font-bold mb-2 md:mb-4 tracking-wider'>
             Contact us
           </h1> */}
-          <div className='flex flex-col gap-2'>
-            {/* <div className='flex gap-2 items-center mb-2'>
+              <div className='flex flex-col gap-2'>
+                {/* <div className='flex gap-2 items-center mb-2'>
               <FaPhone size={20} />
               <Link href={"tel:+9606700804"}>
                 <p className='text-xs md:text-sm'>Phone: +960 </p>
@@ -48,48 +43,39 @@ export default async function Footer() {
                 <p className='text-xs md:text-sm'>Email:</p>
               </Link>
             </div> */}
-          </div>
-        </div>
+              </div>
+            </div>
 
-        <div className='hidden md:flex gap-4 justify-center items-center'>
-          <Link href={"https://www.facebook.com/vaavukeyodhoo"}>
-            <AiFillFacebook size={32} />
-          </Link>
-          <Link href={"https://www.instagram.com/visitkeyodhoo/"}>
-            <AiFillInstagram size={32} />
-          </Link>
-        </div>
+            <div className='hidden md:flex gap-4 justify-center items-center'>
+              <Link href={"https://www.facebook.com/vaavukeyodhoo"}>
+                <AiFillFacebook size={32} />
+              </Link>
+              <Link href={"https://www.instagram.com/visitkeyodhoo/"}>
+                <AiFillInstagram size={32} />
+              </Link>
+            </div>
 
-        <div className='mt-12 md:mt-0 mb-24 md:mb-0'>
-          {/* <h1 className='text-xl font-bold mb-2 md:mb-4 tracking-wider'>
+            <div className='mt-12 md:mt-0 mb-24 md:mb-0'>
+              {/* <h1 className='text-xl font-bold mb-2 md:mb-4 tracking-wider'>
             Quick Links
           </h1> */}
-          <div>
-            {" "}
-            {links &&
-              links.map((link: any, index: number) => (
-                <div key={index}>
-                  <Link
-                    href={link.link ? link.link : "/"}
-                    target='_blank'
-                    className='text-xs lg:text-sm'>
-                    {link.linkName ? link.linkName : ""}
-                  </Link>
-                </div>
-              ))}
+              <div></div>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className='text-center self-center'>
-        <div className=''>
-          <ThemeToggler />
-          <h5 className='text-sm'>© {year} Visitkeyodhoo</h5>
-        </div>
+          <div className='text-center self-center'>
+            <div className=''>
+              <ThemeToggler />
+              <h5 className='text-sm'>© {year} Visitkeyodhoo</h5>
+            </div>
 
-        <h5 className='text-xs text-white/20 dark:text-gray-200/30 mt-2'>
-          Developed by Ahmed Areef
-        </h5>
-      </div>
-    </footer>
+            <h5 className='text-xs text-white/20 dark:text-gray-200/30 mt-2'>
+              Developed by Ahmed Areef
+            </h5>
+          </div>
+        </footer>
+      ) : (
+        ""
+      )}
+    </>
   );
 }
